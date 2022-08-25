8b44e63fc8f86dd3bd3c73fce785575063b9d6ef
marsupialgutz <mars@possums.xyz>
2022-08-25 01:08:43 -0400

    shut up tsserver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
